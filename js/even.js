ff59df0c694c6901a4500d762e9c34e75272550c
CHARLESEmm <ccharlesemmanuel@gmail.com>
2024-12-01 17:36:47 +0100

    feat: pagination ok

--- js/even.js
+++ js/even.js
@@ -1,5 +1,64 @@
-/* pagination gallery d'image */
 
-// je recupere la liste d'images 
+
+
+
+
+
+
+
+// PAGINATION PHOTOS
 const listeImage = document.querySelectorAll('.gallery-item');
+const btnNext = document.querySelector('.next');
+const btnPrev = document.querySelector('.prev');
+const dotsContainer = document.querySelector('.pagination');
 
+let currentIndex = 0;
+
+
+function afficherImage(index) {
+    listeImage.forEach((image, i) => {
+        image.style.display = i === index ? 'block' : 'none';
+    });
+
+   
+    const dots = document.querySelectorAll('.dot');
+    dots.forEach((dot, i) => {
+        dot.classList.toggle('active', i === index);
+    });
+}
+
+// Fonction pour créer les dots
+function createDots() {
+    if (lengthListe > 0) {
+        for (let index = 0; index < lengthListe; index++) {
+            const dot = document.createElement('div');
+            dot.classList.add('dot');
+            if (index === 0) dot.classList.add('active'); 
+            dotsContainer.appendChild(dot);
+
+            
+            dot.addEventListener('click', () => {
+                currentIndex = index;
+                afficherImage(currentIndex);
+            });
+        }
+    }
+}
+
+
+const lengthListe = listeImage.length;
+
+// Gestion des boutons "Suivant" et "Précédent"
+btnNext.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % lengthListe;
+    afficherImage(currentIndex);
+});
+
+btnPrev.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + lengthListe) % lengthListe;
+    afficherImage(currentIndex);
+});
+
+// Initialisation
+createDots();
+afficherImage(currentIndex);
