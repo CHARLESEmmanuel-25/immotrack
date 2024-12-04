db4be9e6499bf9c1055444d39305ff91748cecfe
CHARLESEmm <ccharlesemmanuel@gmail.com>
2024-12-05 00:51:44 +0100

    bdd is fixed

--- App/config/client.js
+++ App/config/client.js
@@ -5,8 +5,8 @@ const client = mariadb.createPool({
     host: 'localhost',
     user: 'root',
     password: '', // Assurez-vous de renseigner un mot de passe sécurisé si nécessaire
-    database: 'suiteGRP',
-    connectionLimit: 5
+    database: 'GRP',
+    
 });
 
 export default client;
